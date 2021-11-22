9880cdae120d969b7bdea655916464532e025978
Stefan Tomic <sttomic@ucdavis.edu>
2021-11-21 23:14:47 -0800

    5.5.4 scrapy mini project

---
+++ mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/toscrape-xpath.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "toscrape-xpath"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.xpath('./span[@class="text"]/text()').get()
+                'author': quote.xpath('.//small[@class="author"]/text()').get(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
